d7bc6be250d293a2a1f76b77a3f06f33f04ac5e4
jeremyPWE <jpwijanto@gmail.com>
2023-04-18 22:14:28 +0700

    delete unused file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="mx-auto my-12 max-w-3xl">
-        <h2 className="text-2xl font-semibold">My shopping list</h2>
+        <div className="flex justify-between">
+          <h2 className="text-2xl font-semibold">My shopping list</h2>
+          <button
+            type="button"
+            className="rounded-md bg-violet-400 p-2 text-sm text-white transition hover:bg-violet-500"
+          >
+            Add shopping item
+          </button>
+        </div>
       </main>
     </>
   );
